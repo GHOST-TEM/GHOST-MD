ee40dc2ceb0c9e322aa7f074ac72571dfb0090b8
GHOST MODZ <ghostbhanu58@gmail.com>
2024-09-24 16:11:03 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&2JqN1AWT",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&gcRm0Egb",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
